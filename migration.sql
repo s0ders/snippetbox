67f05a7ccd893bb02a2a74cc474d0594dd704ac0
s0ders <39492740+s0ders@users.noreply.github.com>
2023-12-26 19:14:08 +0100

    chores: update SQL migration file

--- migration.sql
+++ migration.sql
@@ -10,14 +10,23 @@ CREATE TABLE snippets (
     created DATETIME NOT NULL,
     expires DATETIME NOT NULL
 );
+
 -- Add an index on the created column.
 CREATE INDEX idx_snippets_created ON snippets(created);
 
-CREATE USER 'web'@'localhost';
+CREATE USER 'web' @'localhost';
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON snippetbox.* TO 'web'@'localhost';
+GRANT
+SELECT
+,
+INSERT
+,
+UPDATE
+,
+    DELETE ON snippetbox.* TO 'web' @'localhost';
+
 -- Important: Make sure to swap 'pass' with a password of your own choosing.
-ALTER USER 'web'@'localhost' IDENTIFIED BY 'pass';
+ALTER USER 'web' @'localhost' IDENTIFIED BY 'pass';
 
 CREATE TABLE sessions (
     token CHAR(43) PRIMARY KEY,
@@ -26,3 +35,16 @@ CREATE TABLE sessions (
 );
 
 CREATE INDEX sessions_expiry_idx ON sessions (expiry);
+
+CREATE TABLE users (
+    id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    hashed_password CHAR(60) NOT NULL,
+    created DATETIME NOT NULL
+);
+
+ALTER TABLE
+    users
+ADD
+    CONSTRAINT users_uc_email UNIQUE (email);
